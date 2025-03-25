9ee44aad614d395102dd66879e616bc776681124
khushikumarigupta14 <khushikumarigupta463@gmail.com>
2025-03-25 16:09:18 +0530

    refactor:  add input field

--- src/Convertor.jsx
+++ src/Convertor.jsx
@@ -1,7 +1,7 @@
-// import React, { useState, useCallback, useEffect } from "react";
+import React, { useState } from "react";
 
 const Converter = () => {
-  //   const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
   //   const [fromBase, setFromBase] = useState("decimal");
   //   const [toBase, setToBase] = useState("binary");
   //   const [result, setResult] = useState("");
@@ -13,6 +13,14 @@ const Converter = () => {
         <h1 className="text-2xl font-bold text-gray-800 text-center mb-4">
           Number System Converter
         </h1>
+        {/* Input Field */}
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="w-full p-3 rounded-lg text-lg border border-gray-300 focus:ring-blue-400 focus:outline-none focus:ring-2"
+          placeholder="Enter a number"
+        />
       </div>
     </div>
   );
